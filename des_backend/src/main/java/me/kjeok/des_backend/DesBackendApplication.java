1b77a43c5cf804bbf5cdca81ddde105c5fdf4e96
김지혁 <kjeok00@gmail.com>
2024-12-09 20:01:21 +0900

    add: init settings

---
+++ des_backend/src/main/java/me/kjeok/des_backend/DesBackendApplication.java
@@ -0,0 +1,13 @@
+package me.kjeok.des_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesBackendApplication.class, args);
+	}
+
+}
---
+++ des_backend/src/test/java/me/kjeok/des_backend/DesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kjeok.des_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
