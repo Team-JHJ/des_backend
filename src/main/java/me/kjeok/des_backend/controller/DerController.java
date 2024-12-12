6f838f862b04e9ae74bf116ee2980a33ff477ddf
김지혁 <kjeok00@gmail.com>
2024-12-12 20:05:55 +0900

    update: controller

--- src/main/java/me/kjeok/des_backend/controller/DerController.java
+++ src/main/java/me/kjeok/des_backend/controller/DerController.java
@@ -13,8 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.toList;
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/der")
--- src/main/java/me/kjeok/des_backend/dto/HomeResponse.java
+++ src/main/java/me/kjeok/des_backend/dto/HomeResponse.java
@@ -1,11 +1,13 @@
 package me.kjeok.des_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import me.kjeok.des_backend.domain.*;
 
 @Getter
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class HomeResponse {
     private final Long id;
     private final String homename;
