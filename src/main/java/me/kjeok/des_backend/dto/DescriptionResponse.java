bfbb49a00af6ba250d0b84f7690c97229c71181a
KJK_OMEN\kjk16 <kjeok00@gmail.com>
2024-12-19 13:24:24 +0900

    update: description response에 typeof 추가

--- src/main/java/me/kjeok/des_backend/dto/DescriptionResponse.java
+++ src/main/java/me/kjeok/des_backend/dto/DescriptionResponse.java
@@ -10,10 +10,12 @@ public class DescriptionResponse {
     private final String name;
     private final String description;
     private final Object value;
+    private final String typeof;
 
     public DescriptionResponse(Description description, Object value) {
         this.name = description.getName();
         this.description = description.getDescription();
         this.value = value;
+        this.typeof = value != null ? value.getClass().getSimpleName() : "null";
     }
 }
